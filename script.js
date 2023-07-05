3f293f536811be4f02a9b7c60b44a9e1e6eab985
Emerson Gonzalez <emerson.dvlmt@gmail.com>
2023-07-05 02:38:32 -0400

    Agregué página en inglés y redes sociales

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+function changeLanguage(language) {
+  var currentLanguage = localStorage.getItem('language');
+   if (currentLanguage !== language) {
+    // Guardar preferencia de idioma en las cookies o almacenamiento local
+    // Aquí se utiliza el almacenamiento local como ejemplo
+    localStorage.setItem('language', language);
+     // Redireccionar al usuario a la página correspondiente al idioma seleccionado
+    window.location.href = '/' + language + '/index.html';
+  }
+}
+ // Cargar idioma almacenado previamente en el almacenamiento local (si existe)
+window.addEventListener('DOMContentLoaded', function () {
+  var savedLanguage = localStorage.getItem('language');
+  if (savedLanguage) {
+    changeLanguage(savedLanguage);
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+  var savedLanguage = localStorage.getItem('language');
+  if (!savedLanguage) {
+    // Obtener el idioma del navegador
+    var browserLanguage = navigator.language || navigator.userLanguage;
+     // Redireccionar al usuario al idioma del navegador
+    if (browserLanguage.startsWith('en')) {
+      changeLanguage('en');
+    } else if (browserLanguage.startsWith('es')) {
+      changeLanguage('es');
+    }
+  }
+});
